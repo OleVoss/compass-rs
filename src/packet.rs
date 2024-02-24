7cbc46f76036e3d0722ddad9a786ed6185dadf33
Ole Voß <ole.voss@posteo.de>
2024-02-24 22:19:39 +0100

    hardcoded parsing

--- src/packet.rs
+++ src/packet.rs
@@ -1,8 +1,9 @@
+#[allow(non_snake_case)]
 pub trait FromBytes<In, Out> {
     fn from_bytes(bytes: In) -> Result<Out, std::io::Error>;
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 pub struct CompassHeader {
     pub HALT0: u8,
     pub ROUTESET: u8,
@@ -27,42 +28,36 @@ pub struct CompassHeader {
     pub ZIP: u8,
 }
 
-pub const HEADER_FIELDS: [(&'static str, u8); 21] = [
-    // 1st Byte (mandatory)
-    // it is meant to be disabled. But then table 4.2 is wrong
-    ("HALT0", 1),
-    ("ROUTESET ", 1), // route fmt set
-    ("SIZELEN0 ", 1), // in dombrovski doc this is numbered from 1 which is inconsistent with cs numbering schemes-> we start counting from 0
-    ("ADTN ", 1),     // Answer packet must be DTN
-    ("DTN ", 1),      // use DTN for delivery
-    ("API ", 1),      // service ID is set
-    ("REFSET", 1),    // Reference to other packet. No further doc found about this
-    ("PIDSET", 1), // packet id value is set . makes little sense since it is marked as mandatory in doc....apparently for inter system communication
-    // 2nd byte (optional)
-    ("HALT1 ", 1), // if this is set, this is the last byte from header flag
-    ("API16 ", 1), // if set API field is 2 bytes long instead of 1
-    ("SIZELEN1 ", 1),
-    ("TIME ", 1), // if set Time field is included
-    ("RSV", 2),   // two bits reserved
-    ("CRC ", 1),  // CRC active
-    ("SGN ", 1), // signed (not to be interpreted as cryptographically signed, definitely too short for that)
-    // 3rd byte (even more optional)
-    ("HALT2 ", 1), // no sense to be defined if limit is 3 bytes but here we go
-    ("NULL", 3),   // not defined but not reserved as well
-    ("ERR ", 1),   // payload is service error message
-    ("URG ", 1),   // packet is urgent
-    ("ENC ", 1),   // packet is encrypted
-    ("ZIP ", 1),   // packet is zipped
-];
-
 impl FromBytes<&[u8], CompassHeader> for CompassHeader {
     fn from_bytes(bytes: &[u8]) -> Result<CompassHeader, std::io::Error> {
-        dbg!((bytes[0] >> 7) & CompassHeaderFields::HALT0);
-        Ok(CompassHeader {
-            first_byte: bytes[0],
-            second_byte: bytes[1],
-            third_byte: bytes[2],
-        })
+        let mut header = CompassHeader::default();
+
+        header.ROUTESET = (bytes[0] >> 6) & 1;
+        header.SIZELEN0 = (bytes[0] >> 5) & 1;
+        header.ADTN = (bytes[0] >> 4) & 1;
+        header.DTN = (bytes[0] >> 3) & 1;
+        header.API = (bytes[0] >> 2) & 1;
+        header.REFSET = (bytes[0] >> 1) & 1;
+        header.PIDSET = bytes[0] & 1;
+
+        if ((bytes[0] >> 7) & 1) == 1 {
+            header.API16 = (bytes[1] >> 6) & 1;
+            header.SIZELEN1 = (bytes[1] >> 5) & 1;
+            header.TIME = (bytes[1] >> 4) & 1;
+            header.RSV = (bytes[1] >> 3) & 0b11;
+            header.CRC = (bytes[1] >> 1) & 1;
+            header.SGN = bytes[1] & 1;
+        }
+
+        if ((bytes[1] >> 7) & 1) == 1 {
+            header.NULL = (bytes[2] >> 6) & 1;
+            header.ERR = (bytes[2] >> 4) & 0b111;
+            header.URG = (bytes[2] >> 3) & 1;
+            header.ENC = (bytes[2] >> 2) & 1;
+            header.ZIP = bytes[2] & 1;
+        }
+
+        Ok(header)
     }
 }
 
@@ -84,7 +79,10 @@ mod tests {
 
     #[test]
     fn init_packet() {
-        let bytes = [0b10000001, 0b00000000, 0b00000000];
+        let bytes = [
+            // header                           // payload
+            0b10000000, 0b10000000, 0b10010001, 0b11111111, 0b11111111, 0b11111111,
+        ];
         let packet = CompassPacket::from_bytes(&bytes).unwrap();
         dbg!(packet);
     }
